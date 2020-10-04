17213c4e782703619a9c46b4f9737c2afe6c9d97
ddanddan18 <bob070030@gmail.com>
2020-10-04 16:15:47 +0900

    [feature] login, authericate routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import { ConnectedRouter } from "connected-react-router";
+import Login from "./containers/Login/Login";
+import ArticleList from "./containers/Articles/ArticleList";
+import { connect } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>SWPP HW3!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route path="/login" exact component={Login} />
+            <Redirect exact from="/" to="/login" />
+            <AuthRoute authenticated={this.props.isLogin} path="/articles" exact component={ArticleList} />
+            <AuthRoute authenticated={this.props.isLogin} render={() => <h1>Not Found</h1>} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App;
+// 로그인 되어있지 않으면 로그인 페이지로 보내는 라우팅
+// 로그인 되어있을 경우에는 기존대로 라우팅 (로그인 되어있을 때 로그인 화면에서 articles 로 redirect하는 것은 Login.js에 구현)
+function AuthRoute({ authenticated, component: Component, render, ...rest }) {
+  return <Route {...rest} render={(props) => (authenticated ? render ? render(props) : <Component {...props} /> : <Redirect to={{ pathname: "/login" }} />)} />;
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLogin: state.sign.isLogin,
+  };
+};
+
+export default connect(mapStateToProps, null)(App);
---
+++ src/containers/Login/Login.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router";
+import { connect } from "react-redux";
+import { LOGIN } from "../../store/actions/actionTypes";
+
+class Login extends Component {
+  state = {
+    id: "",
+    pw: "",
+  };
+  render() {
+    if (this.props.isLogin) {
+      return <Redirect to="/articles" />;
+    }
+    return (
+      <div className="Login">
+        <h1>Login</h1>
+        <label>Email ID</label>
+        <input id="email-input" type="text" value={this.state.id} onChange={(event) => this.setState({ id: event.target.value })}></input>
+        <br />
+        <label>Password</label>
+        <input id="pw-input" type="text" value={this.state.pw} onChange={(event) => this.setState({ pw: event.target.value })}></input>
+        <br />
+        <button id="login-button" onClick={() => this.loginHandler()}>
+          Login
+        </button>
+      </div>
+    );
+  }
+
+  loginHandler = () => {
+    // validation login input
+    if (!this.authenticateInput()) return;
+    // change authorize state
+    this.props.onLogin(this.state.id, this.state.pw);
+    // redirect /articles page -> move on render
+  };
+
+  authenticateInput = () => {
+    const isValid = this.state.id === "swpp@snu.ac.kr" && this.state.pw === "iluvswpp";
+    if (!isValid) alert("Email or password is wrong");
+    return isValid;
+  };
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLogin: state.sign.isLogin,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onLogin: (id, pw) => dispatch({ type: LOGIN, id, pw }),
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
